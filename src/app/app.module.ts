a6f317645f0a07103d4f4deba3f14e551d54e67c
Sofia <caniosofia@gmail.com>
2020-06-02 16:53:14 -0300

    Se crean los tres componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LibreriaComponent } from './data/libreria/libreria.component';
+import { LibrosComponent } from './data/libros/libros.component';
+import { TablaComponent } from './data/tabla/tabla.component';
+import { PlanillaComponent } from './data/planilla/planilla.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibreriaComponent,
+    LibrosComponent,
+    TablaComponent,
+    PlanillaComponent
   ],
   imports: [
     BrowserModule,
